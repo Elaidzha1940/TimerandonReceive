5dd7fc89d0650bdb98d07aa012e0fa04536179b0
Elaidzha1940 <eli080293@gmail.com>
2023-11-09 00:23:01 +0300

    TimerandonReceive

---
+++ TimerandonReceive/TimerandonReceiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerandonReceiveApp.swift
+//  TimerandonReceive
+//
+//  Created by Elaidzha Shchukin on 09.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerandonReceiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
