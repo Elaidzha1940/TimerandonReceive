f66c613c1e0f99f4df6c700ae620787e435c7a43
Elaidzha1940 <eli080293@gmail.com>
2023-11-10 18:31:57 +0300

    TimerandonReceive try Countdown to date / timeRemaining

--- TimerandonReceive/ContentView.swift
+++ TimerandonReceive/ContentView.swift
@@ -16,32 +16,38 @@ struct ContentView: View {
     
     // Countdown
     /*
-    @State var count: Int = 10
-    @State var finishedText: String? = nil
-    */
+     @State var count: Int = 10
+     @State var finishedText: String? = nil
+     */
     
     // Countdown to date
-
+    @State var timeRemaining: String = ""
+    let futureDate: Date = Calendar.current.date(byAdding: .day, value: 1, to: Date()) ?? Date()
+    
+    func updateTimeRemaining() {
+        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+        let hour = remaining.hour ?? 0
+        let minute = remaining.minute ?? 0
+        let second = remaining.second ?? 0
+        timeRemaining = "\(hour): \(minute) : \(second)"
+    }
+    
     var body: some View {
         
         ZStack {
-            LinearGradient(colors: [Color.white, Color.gray],
+            LinearGradient(colors: [Color.black, Color.gray],
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
             .ignoresSafeArea()
             
-            Text(finishedText ?? "\(count)")
-                .font(.system(size: 50, weight: .bold, design: .rounded))
+            Text(timeRemaining)
+                .font(.system(size: 50, weight: .bold, design: .monospaced))
                 .foregroundStyle(.white)
                 .lineLimit(1)
                 .minimumScaleFactor(0.1)
         }
         .onReceive(timer, perform: { _ in
-            if count <= 1 {
-                finishedText = "Damn!"
-            } else {
-                count -= 1
-            }
+            updateTimeRemaining()
         })
     }
 }
